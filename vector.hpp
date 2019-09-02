504bd1592521adfa12e5af5f6df74e965a38be4f
gaurav sehgal <gauravsehgal93@gmail.com>
2019-09-02 21:17:59 +0530

    Added reserve()

--- vector.hpp
+++ vector.hpp
@@ -9,31 +9,39 @@ namespace ktd
 		
 		vector() :ptr(nullptr), Capacity(0), NumberOfElements(0) {}
 
-		vector(size_t InitialNumberOfElements)
+		vector(size_t InitialNumberOfElements):ptr(nullptr), Capacity(InitialNumberOfElements), NumberOfElements(0)
 		{
-			Capacity = InitialNumberOfElements;
+			Reserve(Capacity);
+
 			NumberOfElements = InitialNumberOfElements;
-
-			ptr = new(PoolType, Tag) T[Capacity];
-		}
-
-		vector(size_t InitialNumberOfElements, T val)
-		{
-			Capacity = InitialNumberOfElements;
-			NumberOfElements = InitialNumberOfElements;
-
-			ptr = new(PoolType, Tag) T[Capacity];
-			for (auto i = 0UL; i < Capacity; i++)
+			for (auto i = 0UL; i < NumberOfElements; i++)
 			{
-				ptr[i] = val;
+				new (ptr + i) T();
 			}
 		}
 
-		vector(const vector<T, PoolType, Tag> &other) :Capacity(other.Capacity), NumberOfElements(other.NumberOfElements)
+		vector(size_t InitialNumberOfElements, T val):ptr(nullptr), Capacity(InitialNumberOfElements), NumberOfElements(0)
 		{
-			this->ptr = new(PoolType, Tag) T[Capacity];
+			Capacity = InitialNumberOfElements;
+			Reserve(Capacity);
+			NumberOfElements = InitialNumberOfElements;
 			for (auto i = 0UL; i < NumberOfElements; i++)
-				ptr[i] = other.ptr[i];
+			{
+				new (ptr + i) T(val);
+			}
+		}
+
+		vector(const vector<T, PoolType, Tag> &other) :ptr(nullptr), Capacity(other.Capacity), NumberOfElements(0)
+		{
+			if (Capacity > 0)
+			{
+				Reserve(Capacity);
+			}
+					
+			for (auto i = 0UL; i < NumberOfElements; i++)
+			{
+				new (ptr +i) T(other.ptr[i]);
+			}
 		}
 
 		vector(vector<T, PoolType, Tag> &&other) :Capacity(other.Capacity), NumberOfElements(other.NumberOfElements)
@@ -44,12 +52,12 @@ namespace ktd
 
 		vector<T, PoolType, Tag>& operator=(vector<T, PoolType, Tag> &&other)
 		{
-			if (*this != other)
+			if (this != &other)
 			{
 				this->Capacity = other.Capacity;
 				this->NumberOfElements = other.NumberOfElements;
 				this->ptr = other.ptr;
-				other->ptr = nullptr;
+				other.ptr = nullptr;
 			}
 
 			return *this;
@@ -57,12 +65,15 @@ namespace ktd
 
 		vector<T, PoolType, Tag>& operator=(const vector<T, PoolType, Tag> &other)
 		{
-			if (*this != other)
+			if (this != &other)
 			{
 				if (this->Capacity < other.Capacity)
 				{
-					this->Capacity *= 2;
-					Reallocate();
+					this->Capacity = other.Capacity;
+					auto OrigPtr = this->ptr;
+					this->ptr = ReserveOnly(Capacity);
+					Destroy(OrigPtr, this->NumberOfElements);
+					DeAllocate(OrigPtr);
 				}
 
 				this->NumberOfElements = other.NumberOfElements;
@@ -77,45 +88,71 @@ namespace ktd
 
 		~vector()
 		{
-			if (ptr)
-				delete[] ptr;
+			// explicitly call destructors if required.
+			if (!ptr)
+				return;
+
+			for (auto i = 0UL; i < NumberOfElements; i++)
+			{
+				ptr[i].~T();
+			}
+
+			ExFreePool(ptr);
 		}
 
-		void Reallocate()
+		void Reserve(size_t newCapacity)
 		{
 			auto origptr = ptr;
-			ptr = new(PoolType, Tag) T[Capacity];
-			memcpy(ptr, origptr, NumberOfElements * sizeof(T));
+			ptr = static_cast<T*>(ExAllocatePoolWithTag(PoolType, sizeof(T) * newCapacity, Tag));
+		
+			for (auto i = 0UL; i < NumberOfElements; i++)
+			{
+				new (ptr + i) T(origptr[i]);
+			}
+
+			Capacity = newCapacity;
+
 			if (origptr)
-				delete[] origptr;
+			{
+				for (auto i = 0UL; i < NumberOfElements; i++)
+				{
+					origptr[i].~T();
+				}
+
+				ExFreePool(origptr);
+			}
 		}
 
 		void push_back(const T &val)
 		{
+			auto NewCapacity = Capacity;
 			if (NumberOfElements + 1 > Capacity)
 			{
 				// re allocate.
 				if (Capacity == 0)
-					Capacity = 1;
+					NewCapacity = 1;
+				
+				NewCapacity *= 2;
 
-				Capacity *= 2;
-				Reallocate();
+				Reserve(NewCapacity);
 			}
 			
-			ptr[NumberOfElements] = val;
+			new (ptr + NumberOfElements) T(val);
 			NumberOfElements++;
 		}
 
 		void push_back(T &&val)
 		{	
+			auto NewCapacity = Capacity;
 			if (NumberOfElements + 1 > Capacity)
 			{
 				// re allocate.
 				if (Capacity == 0)
-					Capacity = 1;
+					NewCapacity = 1;
 
-				Capacity *= 2;
-				Reallocate();
+				NewCapacity *= 2;
+
+				Reserve(NewCapacity);
 			}
 
 			new (ptr + NumberOfElements) T(move(val));
@@ -125,14 +162,16 @@ namespace ktd
 		template<class ...Args>
 		T& emplace_back(Args&&... args)
 		{
+			auto NewCapacity = Capacity;
 			if (NumberOfElements + 1 > Capacity)
 			{
 				// re allocate.
 				if (Capacity == 0)
-					Capacity = 1;
+					NewCapacity = 1;
 
-				Capacity *= 2;
-				Reallocate();
+				NewCapacity *= 2;
+
+				Reserve(NewCapacity);
 			}
 
 			new (ptr + NumberOfElements) T(forward<Args>(args)...);
@@ -145,7 +184,7 @@ namespace ktd
 			return this->NumberOfElements;
 		}
 
-		T& operator=(size_t index)
+		T& operator[](size_t index)
 		{
 			return ptr[index];
 		}
@@ -155,5 +194,27 @@ namespace ktd
 		T *ptr;
 		size_t Capacity;
 		size_t NumberOfElements;
+
+		T* ReserveOnly(size_t NewCapacity)
+		{
+			return static_cast<T*>(ExAllocatePoolWithTag(PoolType, sizeof(T) * newCapacity, Tag));
+		}
+
+		void Destroy(T *ptr, size_t NumberOfElements)
+		{
+			if (!ptr)
+				return;
+
+			for (auto i = 0UL; i < NumberOfElements; i++)
+			{
+				ptr[i].~T();
+			}
+		}
+
+		void DeAllocate(T* ptr)
+		{
+			if (ptr)
+				ExFreePool(ptr);
+		}
 	};
 }
