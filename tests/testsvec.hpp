ac2bc798d768e262bf161255dff9be445b9a3be9
gaurav sehgal <gauravsehgal93@gmail.com>
2019-12-21 21:16:56 +0530

    Updated tests

---
+++ tests/testsvec.hpp
@@ -0,0 +1,173 @@
+#include <ntddk.h>
+#include "vector.hpp"
+#include "testclass.hpp"
+
+ktd::vector<int, PagedPool, 'cdtd'> *vec = nullptr;
+
+void Test_VecInit()
+{
+	vec = new(PagedPool) ktd::vector<int, PagedPool, 'cdtd'>(10);
+	vec->push_back(1);
+	vec->push_back(2);
+	vec->push_back(3);
+	vec->push_back(4);
+	vec->push_back(5);
+	vec->push_back(6);
+	(*vec)[5] = 98;
+}
+
+void Test_Random()
+{
+	ktd::vector<int, PagedPool, 'tset'> *vect = new(PagedPool) ktd::vector<int, PagedPool, 'tset'>(10);
+	delete vect;
+
+	test *ptr = new(PagedPool) test[2];
+	delete[] ptr;
+}
+
+void Test_Copypod()
+{
+	ktd::vector<int, PagedPool, 'cdtd'> v1;
+	v1.push_back(1);
+	v1.push_back(2);
+	v1.push_back(3);
+	v1.push_back(4);
+	v1.push_back(5);
+
+	ktd::vector<int, PagedPool, 'cdtd'> vec2 = v1;
+	vec2;
+}
+
+void Test_Copynonpod()
+{
+	ktd::vector<test, PagedPool, 'cdtd'> somevec;
+	test obj;
+	somevec.push_back(obj);
+	somevec.push_back(test());
+	somevec.push_back(test());
+	somevec.push_back(test());
+	somevec.push_back(obj);
+	somevec.push_back(obj);
+	somevec.push_back(test());
+	somevec.push_back(test());
+
+	ktd::vector<test, PagedPool, 'cdtd'> vec2 = somevec;
+	//vec2.push_back(obj);
+}
+
+void Test_VecClass()
+{
+	ktd::vector<test, NonPagedPool, 'tset'> vectest;
+	DbgPrint("test size %lu", sizeof(test));
+	vectest.push_back(test());
+
+	test obj;
+	vectest.push_back(obj);
+	vectest.push_back(obj);
+	vectest.push_back(obj);
+	vectest.push_back(obj);
+}
+
+
+void Test_Movepod()
+{
+	ktd::vector<int, PagedPool, 'cdtd'> somevec;
+	somevec.push_back(1);
+	somevec.push_back(2);
+	somevec.push_back(3);
+	somevec.push_back(4);
+	somevec.push_back(5);
+
+	ktd::vector<int, PagedPool, 'cdtd'> vec2 = move(somevec);
+}
+
+void Test_Movenonpod()
+{
+	ktd::vector<test, PagedPool, 'cdtd'> somevec;
+	test obj;
+	somevec.push_back(test());
+	somevec.push_back(test());
+	somevec.push_back(test());
+	somevec.push_back(test());
+	somevec.push_back(obj);
+	somevec.push_back(obj);
+	somevec.push_back(test());
+	somevec.push_back(test());
+
+	ktd::vector<test, PagedPool, 'cdtd'> vec2 = move(somevec);
+}
+
+void Test_Push()
+{
+	ktd::vector<anothertestclass, PagedPool, 'tset'> v1;
+	v1.push_back(1);	//should call constructor and then a move assignment operator.
+}
+
+void Test_Emplace()
+{
+	ktd::vector<anothertestclass, PagedPool, 'tset'> v2;
+	v2.emplace_back(1);	//should call constructor.
+}
+
+void Test_PushVsEmplace()
+{
+	Test_Push();
+	Test_Emplace();
+}
+
+void Test_Reserve()
+{
+	ktd::vector<anothertestclass, PagedPool, 'tset'> v;
+	v.reserve(15);	//should not call construcotrs.
+}
+
+void Test_Default()
+{
+	ktd::vector<test, PagedPool, 'tset'> vect2(2);
+}
+
+void Test_Clear()
+{
+	ktd::vector<int, PagedPool, 'tset'> somevec;
+	somevec.push_back(1);
+	somevec.push_back(1);
+	somevec.push_back(1);
+	somevec.push_back(1);
+	somevec.push_back(1);
+	somevec.push_back(1);
+	DbgPrint("Size : %lu", somevec.size());
+	somevec.Clear();
+	DbgPrint("Size : %lu", somevec.size());
+}
+
+void TestInitVector()
+{
+	Test_Clear();
+
+	Test_Default();
+	Test_Reserve();
+	Test_PushVsEmplace();
+
+	Test_Movepod();
+	Test_Movenonpod();
+
+	Test_Copypod();
+	Test_Copynonpod();
+
+	Test_VecInit();
+	Test_Random();
+	Test_VecClass();
+}
+
+void TestDeInitVector()
+{
+	if (!vec)
+		return;
+
+	for (auto i = 0UL; i < vec->size(); i++)
+	{
+		DbgPrint("%d\n", (*vec)[i]);
+	}
+
+	delete vec;//calls size_t variant of delete.
+}
